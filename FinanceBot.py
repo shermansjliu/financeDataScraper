06baa10c850ca3cb35bd328105f0ddf0537fe2db
Sherman Liu <shermanliu03@gmail.com>
2020-12-29 01:18:23 +0800

    completed extracting all data

--- FinanceBot.py
+++ FinanceBot.py
@@ -2,7 +2,9 @@ from openpyxl import Workbook, load_workbook
 from selenium import webdriver
 from time import sleep
 import re 
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class FinanceBot():
   
@@ -14,11 +16,12 @@ class FinanceBot():
 "Sales/Revenue":0,
 "Cash":0,
 "Total Current Assets":0,
-"short term debt":0,
-"current portion of long term debt":0,
-"total current liabilities":0,
+"Short Term Debt":0,
+"Current Portion of Long Term Debt":0,
+"Total Current Liabilities":0,
 "Net Property, Plant & Equipment":0
 }
+    self.wait = WebDriverWait(self.driver, 10)
   def format_worksheet(self):
     wb = load_workbook(filename="copy_hk_stock.xlsx")
     # print(wb.sheetnames)
@@ -46,26 +49,67 @@ class FinanceBot():
     Extracts pertinent data from the income statement page
     '''
     self.driver.get(f"https://www.wsj.com/market-data/quotes/HK/XHKG/{company_code}/financials/annual/income-statement")
-  
-    sleep(1)
-    #GET sales 
-    sales_revenue = self.driver.find_element_by_xpath('//*[@id="cr_cashflow"]/div[2]/div/table/tbody/tr[1]/td[2]').text
-    sales_revenue = float(sales_revenue)
-    values["Sales/Revenue"] = sales_revenue
+     #get sales/revenue and EBIT
+    sales_revenue = 0 
+    ebit = 0 
+    try:
+        sales_rev_element = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="cr_cashflow"]/div[2]/div/table/tbody/tr[1]/td[2]')))
+        sales_revenue = float(sales_revenue_element.text)
+        #
+        ebit_element = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="cr_cashflow"]/div[2]/div/table/tbody/tr[1]/td[2]')))
+        ebit = float(ebit_element.text)
+    except:
+        self.driver.quit() 
 
-    #Get EBIT
-    
-    ebit = self.driver.find_element_by_xpath('//*[@id="cr_cashflow"]/div[2]/div/table/tbody/tr[56]/td[2]').text
+    values["Sales/Revenue"] = sales_revenue
     values['EBIT'] = float(ebit)
+
+def get_latest_cell(self,row_name)->float:
+  elmt = self.driver.find_element_by_xpath(f"//td[text()='{row_name}']/following-sibling::td[1]")
+  return float(elmt.text)
+
    
 def extract_balance_sheet_data(self, company_code):
   
   self.driver.get(f'https://www.wsj.com/market-data/quotes/HK/XHKG/{company_code}/financials/annual/balance-sheet')
-  cash = self.driver.find_element_by_xpath('//*[@id="cr_cashflow"]/div[2]/div[2]/table/tbody/tr[3]/td[2]').text
+ 
+  try:
+      cash_element = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="cr_cashflow"]/div[2]/div[2]/table/tbody/tr[2]/td[2]')))
+      cash = float(cash_element.text)
+
+      total_current_assets_element = self.wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="cr_cashflow"]/div[2]/div[2]/table/tbody/tr[22]/td[1]')))
+      total_current_assets = float(total_current_assets_element.text)
+
+      #Expand Liabilities & Shareholders' Equity Section
+      liabilities_btn = self.driver.find_element_by_xpath('//*[@id="cr_cashflow"]/div[3]/div[1]')
+      liabilities_btn.click()
+
+      short_term_debt_element = self.driver.find_element_by_xpath("//td[text()='Short Term Debt']/following-sibling::td[1]")
+      short_term_debt = float(short_term_debt_element.text)
+
+
+      lt_debt_elmt = self.driver.find_element_by_xpath("//td[text()='Current Portion of Long Term Debt']/following-sibling::td[1]")
+      lt_debt = float(lt_debt_elmt.text)
+
+      total_curr_liabilites_elmt = self.driver.find_element_by_xpath("//td[text()='Total Current Liabilities']/following-sibling::td[1]")
+      toatl_curr_liabilites = float(toatl_curr_liabilites.text)
+      
+      net_property_and_plan_equipment = self.get_latest_cell("Net Property, Plant & Equipment")
+
+  except:
+      self.driver.quit() 
+
+  '''
+  Net Property, Plant & Equipment
+  '''
   
-  total_current_assets = self.driver.get('//*[@id="cr_cashflow"]/div[2]/div[2]/table/tbody/tr[22]/td[1]').text
-  total_current_assets = float(total_current_assets)
+
   values["Total Current Assets"] = total_current_assets 
+  values["Cash"] = cash
+  values["Short Term Debt"] = short_term_debt
+  values["Current Portion of Long Term Debt"] = lt_debt
+  values["Total Current Liabilities"] = toatl_curr_liabilites
+  values["Net Property, Plant & Equipment"] = net_property_and_plan_equipment
 
   
 
